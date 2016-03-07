ad0541c9629bcf0c5cc5e2749c58431822e7f369
savlu <savanhlu06@yahoo.com>
2016-03-07 17:01:07 -0500

    delete another import and upload javadoc

--- Robot/GUI.java
+++ Robot/GUI.java
@@ -2,7 +2,6 @@ package Robot;
 
 import java.io.File;
 import java.io.IOException;
-import javax.imageio.ImageIO;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
