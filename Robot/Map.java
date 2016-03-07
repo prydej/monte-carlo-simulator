493ffb4f325b7b91f94c238b6aabc17b59087247
Stephen Kristin <sjkristin87@gmail.com>
2016-03-07 16:42:51 -0500

    Fix map again

--- Robot/Map.java
+++ Robot/Map.java
@@ -3,32 +3,42 @@ import java.util.Random;
 
 /**
  * @author Stephen Kristin
- * @version 1.0
- * @last modified 2/29/2016
+ * @version 1.1
+ * @last modified 3/7/2016
+ * 
+ * This class uses random number generation to give x and y values for the reference points
+ * The reference points are simulated by the 2 dimensional array refPoints[][]
+ * 
  */
 
-
 public class Map{
-	
 	// seed random number generation
 	Random gen = new Random();
 
+	// array for reference points
 	public static int[][] refPoints;
 
 	// exception handling 
 	public void finalize() throws Throwable {}
 
-	// create reference points as objects and add to array of objects 
+	/**
+	 * createPoints(int) takes the number of points the user wants from the GUI and gives the points x and y values 
+	 * 
+	 * @param NumberRefPoints is the number of points to be created from the user input on the GUI
+	 * @return void
+	 * 
+	 */
 	public void createPoints(int NumberRefPoints){
 		// variables for loop iteration
 		int i, j;
 		
-		for (i=0; i < NumberRefPoints; i++ ){//NumberRefPoints
+		for (i=0; i < NumberRefPoints; i++ ){// NumberRefPoints is the number from the GUI
 			for (j=0; j<2; j++){
-				refPoints[i][j]= gen.nextInt(100);
+				// j index 0 and 1 for x and y
+				refPoints[i][j]= gen.nextInt(100); 
 			}			
 		}
 	}
-	
+} 
+//end of map class
 
-}
