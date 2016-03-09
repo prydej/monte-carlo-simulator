2bbc15b903bf0bfe164cd7f593ab1d2da5f487ca
prydej <prydej@my.erau.edu>
2016-03-09 10:42:31 -0500

    Update IO.java

--- Robot/IO.java
+++ Robot/IO.java
@@ -1,12 +1,16 @@
-
-package Robot;
-
-
 /**
  * @author julian
  * @version 1.0
+ * @created 13-Feb-2016 1:56:50 PM
  */
-public class IO {
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.layout.Pane;
+
+public class IO extends Application {
 
 	//private String strToSave;
 
@@ -27,5 +31,27 @@ public class IO {
 
 		//String outputString;
 	//}
+	
+	public void start(Stage stage){
+		
+	}
+	
+	public static void setMoveString(String outputString){
+		
+		//Create stage, scene, and pane
+		Pane pane = new Pane();
+		Stage stage = new Stage();
+		Scene scene = new Scene(pane);
+		
+		//Create text object
+		Text runString = new Text();
+		
+		//Set Scene
+		stage.setScene(scene);
+		
+		//add text to pane
+		pane.getChildren().add(runString);
+		
+	}
 
 }
